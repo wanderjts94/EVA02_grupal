3567c9585c2527dd31ac1a3764881958785d4155
LeonardoGrandeH <SM75119591@idat.edu.pe>
2024-02-24 18:54:42 -0500

    commit Grande 01

---
+++ app/src/main/java/pe/edu/idat/eva02_grupal/Ejercicio02_Lista.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.eva02_grupal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio02_Lista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio02_lista)
+    }
+}
