285cd8fd4725e177e9b24ae92fd4204f7a62286e
wanderjts94 <jhontitosalcedo94@gmail.com>
2024-02-25 04:33:55 -0500

    tito 4

--- app/src/main/java/pe/edu/idat/eva02_grupal/Ejercicio01.kt
+++ app/src/main/java/pe/edu/idat/eva02_grupal/Ejercicio01.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.widget.CheckBox
 import pe.edu.idat.eva02_grupal.databinding.ActivityEjercicio01Binding
 import android.content.Intent
+import android.os.Handler
 import pe.edu.idat.eva02_grupal.otros.AppMensaje
 import pe.edu.idat.eva02_grupal.otros.TipoMensaje
+import android.util.Patterns
+
 
 import java.util.ArrayList
 
@@ -20,10 +23,11 @@ class Ejercicio01 : AppCompatActivity(), View.OnClickListener {
         binding = ActivityEjercicio01Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnacceder.setOnClickListener(this)
-        binding.rbIng.setOnClickListener(this)
-        binding.rbAdmin.setOnClickListener(this)
-        binding.rbCienMet.setOnClickListener(this)
-        binding.rbOtros.setOnClickListener(this)
+        binding.btnregresar1.setOnClickListener(this)
+        binding.chbPruntual.setOnClickListener(this)
+        binding.chbresponsable.setOnClickListener(this)
+        binding.chbRespetuoso.setOnClickListener(this)
+        binding.chbOTros.setOnClickListener(this)
     }
 
     override fun onClick(vista: View) {
@@ -32,45 +36,70 @@ class Ejercicio01 : AppCompatActivity(), View.OnClickListener {
         } else {
             when (vista.id) {
                 R.id.btnacceder -> {
-                    registrarregistros()
-                    startActivity(Intent(applicationContext, ListadoActivity::class.java).apply {
-                        putExtra("listaregistros", listaregistros)
-                    })
+                    if (validarregistro()) {
+                        registrarregistros()
+                    }
+                }
+                R.id.btnregresar1 -> {
+                // Redirect
+                val intent = Intent(this, PaginaPrincipal::class.java)
+                startActivity(intent)
                 }
             }
         }
     }
 
-
-
     private fun agregarQuitarcualidades(vista: CheckBox) {
-
         if(vista.isChecked) {
             when (vista.id) {
-                R.id.ckPuntual -> listacualidades.add(vista.text.toString())
-                R.id.ckResp -> listacualidades.add(vista.text.toString())
-                R.id.ckResponsab -> listacualidades.add(vista.text.toString())
-                R.id.ckotro -> listacualidades.add(vista.text.toString())
+                R.id.chbPruntual -> listacualidades.add(vista.text.toString())
+                R.id.chbresponsable -> listacualidades.add(vista.text.toString())
+                R.id.chbRespetuoso -> listacualidades.add(vista.text.toString())
+                R.id.chbOTros -> listacualidades.add(vista.text.toString())
             }
-        }else{
+        } else {
             when(vista.id){
-                R.id.ckResponsab -> listacualidades.remove(vista.text.toString())
-                R.id.ckResp -> listacualidades.remove(vista.text.toString())
-                R.id.ckPuntual -> listacualidades.remove(vista.text.toString())
-                R.id.ckotro -> listacualidades.remove(vista.text.toString())
+                R.id.chbPruntual -> listacualidades.remove(vista.text.toString())
+                R.id.chbresponsable -> listacualidades.remove(vista.text.toString())
+                R.id.chbRespetuoso -> listacualidades.remove(vista.text.toString())
+                R.id.chbOTros -> listacualidades.remove(vista.text.toString())
             }
         }
     }
+
     fun registrarregistros() {
         if (validarregistro()) {
-            val inforegistro = "${binding.tvnombre.text.toString()} ${binding.tvapellido.text.toString()} ${binding.tvdni.text.toString()} ${binding.tvcelular.text.toString()}" +
-                    " ${binding.tvemail.text} ${obtenercarreraseleccionada()} ${obtenercualidades()}"
-            listaregistros.add(inforegistro)
+            val infopersona = binding.tvnombre.text.toString() + " " +
+                    binding.tvapellido.text.toString() + " " +
+                    binding.tvdni.text.toString() + " " +
+                    binding.tvcelular.text.toString() + " " +
+                    binding.tvemail.text.toString() + " " +
+                    obtenercarreraseleccionada() + " " +
+                    obtenercualidades()
+            listaregistros.add(infopersona)
             limpiarControles()
+            Handler().postDelayed({
+                startActivity(Intent(applicationContext, ListadoActivity::class.java).apply {
+                    putExtra("listaregistros", listaregistros)
+                })
+            }, 5000)
         }
     }
 
-
+    fun obtenercarreraseleccionada(): String {
+        var carrera = ""
+        val radioButtonId = binding.rgcarreras.checkedRadioButtonId
+        if (radioButtonId != -1) {
+            carrera = when (radioButtonId) {
+                R.id.rbIng -> binding.rbIng.text.toString()
+                R.id.rbAdmin -> binding.rbAdmin.text.toString()
+                R.id.rbCienMet -> binding.rbCienMet.text.toString()
+                R.id.rbOtros -> binding.rbOtros.text.toString()
+                else -> ""
+            }
+        }
+        return carrera
+    }
 
     private fun obtenercualidades():String{
         var cualidades=""
@@ -78,33 +107,43 @@ class Ejercicio01 : AppCompatActivity(), View.OnClickListener {
             cualidades+= "$cual -"
         }
         return cualidades
-
     }
+
     private fun limpiarControles() {
-        listaregistros.clear()
+        listacualidades.clear()
         binding.tvnombre.setText("")
         binding.tvapellido.setText("")
         binding.tvdni.setText("")
         binding.tvcelular.setText("")
         binding.tvemail.setText("")
-        binding.ckPuntual.isChecked=false
-        binding.ckResp.isChecked=false
-        binding.ckResponsab.isChecked=false
-        binding.ckotro.isChecked=false
+        binding.chbPruntual.isChecked=false
+        binding.chbresponsable.isChecked=false
+        binding.chbRespetuoso.isChecked=false
+        binding.chbOTros.isChecked=false
         binding.rgcarreras.clearCheck()
         binding.tvnombre.isFocusableInTouchMode=true
         binding.tvnombre.requestFocus()
     }
 
     fun validarcualidades():Boolean{
-        var repuesta=true
-        if(binding.ckPuntual.isChecked || binding.ckResp.isChecked || binding.ckResponsab.isChecked|| binding.ckotro.isChecked){
-            repuesta=true
+        var respuesta=true
+        if(binding.chbPruntual.isChecked || binding.chbresponsable.isChecked || binding.chbRespetuoso.isChecked|| binding.chbOTros.isChecked){
+            respuesta=true
         }
-        return repuesta
+        return respuesta
     }
 
+    fun validarDNI(dni: String): Boolean {
+        return dni.length == 8 && dni.matches("\\d+".toRegex())
+    }
 
+    fun validarCorreo(correo: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(correo).matches()
+    }
+
+    fun validarEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 
     fun validarCarrera(): Boolean {
         var respuesta = false
@@ -116,10 +155,10 @@ class Ejercicio01 : AppCompatActivity(), View.OnClickListener {
         }
         return respuesta
     }
+
     fun validarnombreapp():Boolean{
         var respuesta=true
         if(binding.tvnombre.text.toString().trim().isEmpty()){
-
             binding.tvnombre.isFocusableInTouchMode = true
             binding.tvnombre.requestFocus()
             respuesta=false
@@ -131,41 +170,29 @@ class Ejercicio01 : AppCompatActivity(), View.OnClickListener {
         return respuesta
     }
 
-
     fun validarregistro(): Boolean {
         var respuesta = false
         if (!validarnombreapp()) {
             AppMensaje.enviarmensaje(binding.root, "Ingrese Nombre y Apellido", TipoMensaje.ERROR)
-        }
-        else if (!validarCarrera()) {
-            AppMensaje.enviarmensaje(binding.root, "Ingrese genero", TipoMensaje.ERROR)
-        }
-        else if (!validarcualidades()) {
-            AppMensaje.enviarmensaje(binding.root, "Ingrese preferencia", TipoMensaje.ERROR)
-        }
-        else {
+        }else if (!validarDNI(binding.tvdni.text.toString())) {
+            AppMensaje.enviarmensaje(binding.root, "DNI inválido", TipoMensaje.ERROR)
+        } else if (!validarCorreo(binding.tvemail.text.toString())) {
+            AppMensaje.enviarmensaje(binding.root, "Correo inválido", TipoMensaje.ERROR)
+        } else if (!validarEmail(binding.tvemail.text.toString())) {
+            AppMensaje.enviarmensaje(binding.root, "Email inválido", TipoMensaje.ERROR)
+        }  else if (!validarCarrera()) {
+            AppMensaje.enviarmensaje(binding.root, "seleccione  carrera", TipoMensaje.ERROR)
+        } else if (!validarcualidades()) {
+            AppMensaje.enviarmensaje(binding.root, "seleccione cualidades", TipoMensaje.ERROR)
+        } else {
             respuesta = true
         }
         return respuesta
     }
 
-    fun obtenercarreraseleccionada():String {
-        var carrera = ""
-        when (binding.rgcarreras.checkedRadioButtonId) {
-            R.id.rbIng -> {
-                carrera = binding.rbIng.text.toString()
-            }
-
-            R.id.rbAdmin -> {
-                carrera = binding.rbAdmin.text.toString()
-            }
-            R.id.rbCienMet -> {
-                carrera = binding.rbCienMet.text.toString()
-            }
-            R.id.rbOtros -> {
-                carrera = binding.rbOtros.text.toString()
-            }
-        }
-        return carrera
-    }
 }
+
+
+
+
+
--- app/src/main/java/pe/edu/idat/eva02_grupal/ListadoActivity.kt
+++ app/src/main/java/pe/edu/idat/eva02_grupal/ListadoActivity.kt
@@ -6,19 +6,30 @@ import android.view.View
 import android.widget.ArrayAdapter
 import pe.edu.idat.eva02_grupal.databinding.ActivityListadoBinding
 import android.R
+import android.content.Intent
+
 class ListadoActivity : AppCompatActivity(), View.OnClickListener  {
     private lateinit var binding: ActivityListadoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListadoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val listaregistros= intent.getSerializableExtra("listaregistros")
+        val Listaregistar = intent.getSerializableExtra("listaregistros")
                 as ArrayList<String>
-        val adapter= ArrayAdapter(applicationContext,R.layout.simple_list_item_1,listaregistros)
-        binding.lstregist.adapter=adapter
+        val adapter = ArrayAdapter(applicationContext,
+            R.layout.simple_list_item_1,
+            Listaregistar)
+        binding.lstregist.adapter = adapter
+        binding.btnvol.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(vista: View) {
+        when (vista.id) {
+            pe.edu.idat.eva02_grupal.R.id.btnvol -> {
+                // Redirect to Ejercicio01Activity
+                val intent = Intent(this, Ejercicio01::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
